867e21b633c2f9d7bd9f58d33a1797a7e0b74bb7
iamgauraav <iamgauraav@gmail.com>
2022-03-22 22:19:11 +0530

    Added Welcome message to master branch

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented program on master branch");
+            Console.ReadLine();
+        }
+    }
+}
